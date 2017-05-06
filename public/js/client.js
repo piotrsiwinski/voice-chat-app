4e30a8fe4efbaf86f0f3ba6c6baedda4c2ec56da
Piotr <siwinski.piotr@outlook.com>
2017-05-06 21:33:05 +0200

    Start chat button

--- public/js/client.js
+++ public/js/client.js
@@ -1,49 +1,110 @@
-var socket = io.connect('http://localhost:3000');
+$(document).ready(onDocumentReady);
+function onDocumentReady(){
+    $("#start-chat-button").click(startChatOnClick);
+}
 
-/***********************************************************************************************************************/
-/* DOSTĘP DO MULTIMEDIÓW - UTWORZENIE LOKALNEGO STREAMU I WYŚWIETLENIE GO */
+function startChatOnClick() {
+    var socket = io.connect('http://localhost:3000');
 
-navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true
-  })
-  .then(function(stream) { 
-    document.getElementById("localVideo").srcObject = stream;
-    window.localStream = stream;
+    /***********************************************************************************************************************/
+    /* DOSTĘP DO MULTIMEDIÓW - UTWORZENIE LOKALNEGO STREAMU I WYŚWIETLENIE GO */
 
-    socket.emit('hello', 'Test'); // <-- 'Test' - nazwa pokoju
-  })
-  .catch(function(e) {
-    console.log('getUserMedia() error: ', e);
-  });
+    navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true
+    })
+        .then(function (stream) {
+            document.getElementById("localVideo").srcObject = stream;
+            window.localStream = stream;
 
-/***********************************************************************************************************************/
-/* 
-    OTRZYMANIE IDENTYFIKATORÓW DOŁĄCZONYCH DO SERWERA KLIENTÓW - UTWORZENIE LICZBY PEERÓW ODPOWIADAJĄCEJ ICH ILOŚCI -
-    WYSŁANIE OFERTY POŁĄCZENIA DO KAŻDEGO Z DOWIĄZANYCH DO SERWERA KLIENTÓW KORZYSTAJĄC POJEDYNCZO Z UTWORZONYCH WCZEŚNIEJ NA TEN CEL PEERÓW
-*/
+            socket.emit('hello', 'Test'); // <-- 'Test' - nazwa pokoju
+        })
+        .catch(function (e) {
+            console.log('getUserMedia() error: ', e);
+        });
 
-var peers = new Array();
+    /***********************************************************************************************************************/
+    /*
+     OTRZYMANIE IDENTYFIKATORÓW DOŁĄCZONYCH DO SERWERA KLIENTÓW - UTWORZENIE LICZBY PEERÓW ODPOWIADAJĄCEJ ICH ILOŚCI -
+     WYSŁANIE OFERTY POŁĄCZENIA DO KAŻDEGO Z DOWIĄZANYCH DO SERWERA KLIENTÓW KORZYSTAJĄC POJEDYNCZO Z UTWORZONYCH WCZEŚNIEJ NA TEN CEL PEERÓW
+     */
 
-socket.on('hello', function(clientsNames) {
+    var peers = new Array();
 
-    clientsNames.forEach(function(client) {
+    socket.on('hello', function (clientsNames) {
 
-        let conn = new webkitRTCPeerConnection({"iceServers": [{ "url": "stun:stun.1.google.com:19302" }]}, { 
-            optional: [{RtpDataChannels: true}] 
+        clientsNames.forEach(function (client) {
+
+            let conn = new webkitRTCPeerConnection({"iceServers": [{"url": "stun:stun.1.google.com:19302"}]}, {
+                optional: [{RtpDataChannels: true}]
+            });
+
+            conn.addStream(window.localStream);
+
+            conn.createOffer({offerToReceiveAudio: 1, offerToReceiveVideo: 1}).then(function (offer) {
+                    conn.setLocalDescription(offer);
+
+                    peers.push(conn);
+                    let n = peers.indexOf(conn);
+
+                    peers[n].onaddstream = function (e) {
+
+                        let videoElement = document.createElement('video');
+                        videoElement.setAttribute("class", "remoteVideo");
+                        videoElement.setAttribute("id", "video_Socket_" + n);
+                        videoElement.setAttribute("autoplay", "autoplay");
+                        videoElement.setAttribute("controls", "controls");
+                        videoElement.setAttribute("width", "240");
+                        videoElement.setAttribute("height", "240");
+
+                        videoElement.srcObject = e.stream;
+                        document.getElementById("remotes").appendChild(videoElement);
+                    }
+
+                    socket.emit('offer', {oferta: offer, cel: client, od: socket.id, index: n});
+                },
+                function (error) {
+                    alert("Błąd: " + error);
+                });
+
+        }, this);
+    });
+
+    /***********************************************************************************************************************/
+    /*
+     OTRZYMANIE NADESŁANEJ PRZEZ INNEGO KLIENTA OFERTY - DLA OTRZYMANEJ OFERTY TWORZONY JEST NOWY PEER DO KTÓREGO DOWIĄZYWANY JEST LOKALNY STREAM
+     WYGENEROWANIE ODPOWIEDZI DLA KLIENTA KTÓRY WYSŁAŁ DO NAS OFERTĘ I WYSŁANIE JEJ DO TEGO KLIENTA WSKAZUJĄC KONKRETNY PEER PRZECHOWYWANY PRZEZ TEGO KLIENTA
+     */
+    socket.on('offer', function (dane) {
+
+        let conn = new webkitRTCPeerConnection({"iceServers": [{"url": "stun:stun.1.google.com:19302"}]}, {
+            optional: [{RtpDataChannels: true}]
         });
 
         conn.addStream(window.localStream);
 
-        conn.createOffer({offerToReceiveAudio: 1, offerToReceiveVideo: 1}).then(function (offer) { 
-            conn.setLocalDescription(offer);
+        conn.setRemoteDescription(new RTCSessionDescription(dane.oferta));
+        conn.createAnswer().then(function (answer) {
 
-            peers.push(conn);   
-            let n = peers.indexOf(conn);
+                conn.setLocalDescription(answer);
 
-            peers[n].onaddstream = function (e) {
+                conn.onicecandidate = function (event) {
+                    if (event.candidate) {
+                        socket.emit('candidate', {
+                            kandydat: event.candidate,
+                            cel: dane.od,
+                            od: socket.id,
+                            index: dane.index
+                        });
+                    }
+                };
 
-                let videoElement = document.createElement('video');
+                peers.push(conn);
+                let n = peers.indexOf(conn);
+
+                peers[n].onaddstream = function (e) {
+
+                    let videoElement = document.createElement('video');
                     videoElement.setAttribute("class", "remoteVideo");
                     videoElement.setAttribute("id", "video_Socket_" + n);
                     videoElement.setAttribute("autoplay", "autoplay");
@@ -51,98 +112,50 @@ socket.on('hello', function(clientsNames) {
                     videoElement.setAttribute("width", "240");
                     videoElement.setAttribute("height", "240");
 
-                videoElement.srcObject = e.stream;
-                document.getElementById("remotes").appendChild(videoElement);
-            }
+                    videoElement.srcObject = e.stream;
+                    document.getElementById("remotes").appendChild(videoElement);
+                }
 
-            socket.emit('offer', {oferta: offer, cel: client, od: socket.id, index: n});
-        },
-        function (error) {
-            alert("Błąd: " + error);
-        });
-        
-    }, this);
-});
-
-/***********************************************************************************************************************/
-/*
-    OTRZYMANIE NADESŁANEJ PRZEZ INNEGO KLIENTA OFERTY - DLA OTRZYMANEJ OFERTY TWORZONY JEST NOWY PEER DO KTÓREGO DOWIĄZYWANY JEST LOKALNY STREAM
-    WYGENEROWANIE ODPOWIEDZI DLA KLIENTA KTÓRY WYSŁAŁ DO NAS OFERTĘ I WYSŁANIE JEJ DO TEGO KLIENTA WSKAZUJĄC KONKRETNY PEER PRZECHOWYWANY PRZEZ TEGO KLIENTA
-*/
-socket.on('offer', function (dane) {
-
-    let conn = new webkitRTCPeerConnection({"iceServers": [{ "url": "stun:stun.1.google.com:19302" }]}, { 
-        optional: [{RtpDataChannels: true}] 
+                socket.emit('answer', {odpowiedz: answer, cel: dane.od, od: socket.id, index: dane.index, indexZewn: n});
+            },
+            function (error) {
+                alert("Błąd: " + error);
+            });
     });
 
-    conn.addStream(window.localStream);
+    /***********************************************************************************************************************/
+    /* OTRZYMANO DANE KONFIGURACYJNE OD KLIENTA Z KTÓRYM NAWIĄZANO POŁĄCZENIE */
 
-    conn.setRemoteDescription(new RTCSessionDescription(dane.oferta));
-    conn.createAnswer().then(function (answer) {
+    socket.on('candidate', function (dane) {
+        (peers[dane.index]).addIceCandidate(new RTCIceCandidate(dane.kandydat));
+    });
 
-        conn.setLocalDescription(answer);
+    /***********************************************************************************************************************/
+    /* OTRZYMANO ODPOWIEDŹ OD KLIENTA DO KTÓREGO WCZEŚNIEJ WYSŁANA ZOSTAŁA OFERTA */
 
-        conn.onicecandidate = function (event) { 
-            if (event.candidate) {
-                socket.emit('candidate', {kandydat: event.candidate, cel: dane.od, od: socket.id, index: dane.index});
-            }
-        };
+    socket.on('answer', function (dane) {
+        (peers[dane.index]).setRemoteDescription(new RTCSessionDescription(dane.odpowiedz));
+    });
 
-        peers.push(conn);
-        let n = peers.indexOf(conn);
+    /***********************************************************************************************************************/
+    /* OTRZYMANO INFORMACJĘ Z SERWERA KTÓRY PEER NALEŻY WYŁĄCZYĆ A PODGLĄD STREAMA UKRYĆ PONIEWAŻ KLIENT TEN SIĘ ROZŁĄCZYŁ Z SERWEREM */
 
-        peers[n].onaddstream = function (e) {
+    socket.on('disconn', function (n) {
 
-            let videoElement = document.createElement('video');
-                videoElement.setAttribute("class", "remoteVideo");
-                videoElement.setAttribute("id", "video_Socket_" + n);
-                videoElement.setAttribute("autoplay", "autoplay");
-                videoElement.setAttribute("controls", "controls");
-                videoElement.setAttribute("width", "240");
-                videoElement.setAttribute("height", "240");
+        peers[n].close();
+        peers[n] = null;
 
-            videoElement.srcObject = e.stream;
-            document.getElementById("remotes").appendChild(videoElement);
-        }
+        let remoteVideos = document.getElementById("remotes");
+        let videoElement = document.getElementById("video_Socket_" + n);
 
-        socket.emit('answer', {odpowiedz: answer, cel: dane.od, od: socket.id, index: dane.index, indexZewn: n});		
-   }, 
-   function (error) {
-      alert("Błąd: " + error);
-   });
-});
+        remoteVideos.removeChild(videoElement);
+    });
 
-/***********************************************************************************************************************/
-/* OTRZYMANO DANE KONFIGURACYJNE OD KLIENTA Z KTÓRYM NAWIĄZANO POŁĄCZENIE */
+    /***********************************************************************************************************************/
+    /* PROŚBA O POTWIERDZENIE CHĘCI WYJŚCIA BĄDŹ PRZEŁADOWANIA STRONY */
 
-socket.on('candidate', function (dane) {
-    (peers[dane.index]).addIceCandidate(new RTCIceCandidate(dane.kandydat));
-});
-
-/***********************************************************************************************************************/
-/* OTRZYMANO ODPOWIEDŹ OD KLIENTA DO KTÓREGO WCZEŚNIEJ WYSŁANA ZOSTAŁA OFERTA */
-
-socket.on('answer', function(dane){
-    (peers[dane.index]).setRemoteDescription(new RTCSessionDescription(dane.odpowiedz));
-});
-
-/***********************************************************************************************************************/
-/* OTRZYMANO INFORMACJĘ Z SERWERA KTÓRY PEER NALEŻY WYŁĄCZYĆ A PODGLĄD STREAMA UKRYĆ PONIEWAŻ KLIENT TEN SIĘ ROZŁĄCZYŁ Z SERWEREM */
-
-socket.on('disconn', function(n) {
-
-    peers[n].close();
-    peers[n] = null;
-
-    let remoteVideos = document.getElementById("remotes");
-    let videoElement = document.getElementById("video_Socket_" + n);
-
-    remoteVideos.removeChild(videoElement);
-});
-
-/***********************************************************************************************************************/
-/* PROŚBA O POTWIERDZENIE CHĘCI WYJŚCIA BĄDŹ PRZEŁADOWANIA STRONY */
-
-window.onbeforeunload = function(event) {
-    return 'Zamknięcie konwersacji';
+    window.onbeforeunload = function (event) {
+        return 'Zamknięcie konwersacji';
+    }
 }
+
--- public/js/site.js
+++ public/js/site.js
@@ -1,7 +1,3 @@
 /*Custom site js*/ 
-$(document).ready(function(){
-    $("#buttton").click(function(){
-        console.log("Click works");
-    });
-});
+
 
