9f16ff256d5197844ff2b8e4f74bb0669bc36b5b
Piotr <siwinski.piotr@outlook.com>
2017-05-06 23:51:58 +0200

    Added production, development and test enviroment

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -36,13 +38,11 @@ app.use(function(req, res, next) {
 });
 
 
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  res.locals.error = req.app.get('env') === 'development' ? err : err.status;
   // render the error page
   res.status(err.status || 500);
   res.render('shared/error');
